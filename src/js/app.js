78e3ba363b86b32788288f0b0eaeb6501e4df934
stevenjohs <johannessteven9@gmail.com>
2021-02-24 19:37:33 +0700

    init commit

---
+++ src/js/app.js
@@ -0,0 +1,26 @@
+// helpers
+const elQS = el => document.querySelector(el);
+const elQSA = el => document.querySelectorAll(el);
+const on = (el, event, val) => el.addEventListener(event, val);
+
+// variables
+
+// configs
+
+// functions
+
+// main
+window.addEventListener('load', init);
+
+function init() {
+    load();
+    eventListener();
+}
+
+function load() {
+
+}
+
+function eventListener() {
+
+}
